53ee5cfbcdb10af1a26cb1aa4a381ea0539dd6a3
Kolappan Nathan <kolappan.vertisize@outlook.com>
2019-07-03 15:17:03 +0530

    Completed #12

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ## [Unreleased]
 ### Added
  - New logger config object
+ - New API to test logger
 
 ## [1.0.2] - 2019-06-28
 ### Added
---
+++ src/Logger.API/Controllers/LoggerController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Logger.API.Controllers
+{
+    [Route("api/logger")]
+    [ApiController]
+    public class LoggerController : ControllerBase
+    {
+        [HttpGet("")]
+        public ActionResult<string> Get()
+        {
+            var logger = new nk.logger.csv.Logger("yyyy-MM-dd HH:mm:ss.fff", "ErrorLog");
+            var exception = new ArgumentNullException("arg-1");
+            logger.Error(exception);
+            logger.Info("sample info text");
+            return "Completed";
+        }
+    }
+}
