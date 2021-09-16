e930d9c97dcd5d9829308245804f28b3e665ac36
Kolappan Nathan <kolappannathan@users.noreply.github.com>
2021-09-16 21:48:50 +0530

    replacing API for manual testing with console app

---
+++ src/Logger.ManualTest/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Logger.ManualTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Initialization
+
+            var config = new nk.logger.csv.LoggerConfig()
+                .SetReplacementValue(';')
+                .SetRelativePath("logs");
+            var logger = new nk.logger.csv.Logger(config);
+
+            #endregion Initialization
+
+            #region Logging
+
+            // inner exception should be logged
+            var exception = new ArgumentNullException("arg-1", new Exception("args-2"));
+            logger.Error(exception);
+
+            // comma should not be logged
+            logger.Info("sample info text,");
+
+            // should not be logged
+            logger.Error("");
+
+            // should not be logged
+            logger.Fatal(ex: null);
+
+            #endregion Logging
+
+            Console.WriteLine("Completed Test");
+            Console.WriteLine("Check if the Excel file is created");
+        }
+    }
+}
